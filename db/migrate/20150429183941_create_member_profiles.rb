4042b7e359bba932ee0170bd8a03eeb945be5095
James Parseghian <jjparseghian@gmail.com>
2015-04-29 17:08:13 -0400

    Add member profile migration

---
+++ db/migrate/20150429183941_create_member_profiles.rb
@@ -0,0 +1,27 @@
+class CreateMemberProfiles < ActiveRecord::Migration
+  def change
+    create_table :member_profiles do |t|
+      t.string :ethnicity
+      t.string :height
+      t.string :weight
+      t.string :smoking_status
+      t.string :activity_level
+      t.text :training_location, array: true, default: []
+      t.text :my_own_space
+      t.text :independent_facility
+      t.text :trainer_facility
+      t.text :fitness_facility
+      t.text :goals, array: true, default: []
+      t.text :goal_specifics
+      t.text :medical_conditions, array: true, default: []
+      t.text :past_injury, array: true, default: []
+      t.text :injury_history, array: true, default: []
+      t.text :training_style, array: true, default: []
+      t.string :appointment_lengths, array: true, default: []
+      t.string :group_training, array: true, default: []
+      t.boolean :consent_waiver
+
+      t.timestamps null: false
+    end
+  end
+end
