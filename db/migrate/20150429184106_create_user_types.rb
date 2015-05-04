f83e610aefc625dba4008d83d15650afb0b52d16
James Parseghian <jjparseghian@gmail.com>
2015-05-04 00:34:26 -0400

    Add foreign key to user type table

--- db/migrate/20150429184106_create_user_types.rb
+++ db/migrate/20150429184106_create_user_types.rb
@@ -2,9 +2,10 @@ class CreateUserTypes < ActiveRecord::Migration
   def change
     create_table :user_types do |t|
       t.string :type
-      t.references :user
+      t.references :user, index: true
 
       t.timestamps null: false
     end
+    add_foreign_key :user_types, :users
   end
 end
