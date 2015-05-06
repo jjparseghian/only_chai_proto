18cc122a4c84db15773afe29585d8870e8fca01c
James Parseghian <jjparseghian@gmail.com>
2015-05-05 20:05:55 -0400

    Fix profile type conditional and make strong params more readable

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -15,7 +15,7 @@ class ProfilesController < ApplicationController
       else
         p "Something went wrong"
       end
-    elsif user.user_type == "Fitness Professional"
+    elsif user.user_type == "Fitness_Professional"
       @fitness_professional = FitnessProfessionalProfile.new(fitness_professional_params)
       if @fitness_professional.save
         p "Successfully created fitness professional profile"
@@ -40,29 +40,50 @@ class ProfilesController < ApplicationController
   private
 
   def client_params
-    params.require(:client).permit(:ethnicity,
-                                    :height,
-                                    :weight,
-                                    :smoking_status,
-                                    :activity_level,
-                                    :own_space_descrip,
-                                    :independent_facility_descrip,
-                                    :trainer_facility_descrip,
-                                    :fitness_facility_descrip,
-                                    :goal_specifics,
-                                    :consent_waiver,
-                                    :user_id,
-                                    :injury_history => [],
-                                    :past_injury => [],
-                                    :medical_conditions => [],
-                                    :goals => [],
-                                    :training_location => [],
-                                    :training_style => [],
-                                    :appointment_lengths => [],
-                                    :group_training => [])
+    params.require(:client).permit(
+      :ethnicity,
+      :height,
+      :weight,
+      :smoking_status,
+      :activity_level,
+      :own_space_descrip,
+      :independent_facility_descrip,
+      :trainer_facility_descrip,
+      :fitness_facility_descrip,
+      :goal_specifics,
+      :consent_waiver,
+      :user_id,
+      :injury_history => [],
+      :past_injury => [],
+      :medical_conditions => [],
+      :goals => [],
+      :training_location => [],
+      :training_style => [],
+      :appointment_lengths => [],
+      :group_training => []
+      )
   end
 
   def fitness_professional_params
-    params.require(:fitness_professional).permit(:ethnicity, :training_location, :client_space_descrip, :independent_facility_descrip, :membership_facility_descrip, :private_facility_descrip, :credentials, :goals, :goal_specifics, :medical_conditions, :orthopedic_injury_experience, :population_experience, :athlete_experience, :training_style, :appointment_lengths, :group_training, :consent_waiver, :user_id)
+    params.require(:fitness_professional).permit(
+      :ethnicity,
+      :client_space_descrip,
+      :independent_facility_descrip,
+      :membership_facility_descrip,
+      :private_facility_descrip,
+      :credentials,
+      :goal_specifics,
+      :athlete_experience,
+      :consent_waiver,
+      :user_id,
+      :training_location => [],
+      :goals => [],
+      :medical_conditions => [],
+      :orthopedic_injury_experience => [],
+      :population_experience => [],
+      :training_style => [],
+      :appointment_lengths => [],
+      :group_training => [],
+      )
   end
 end
