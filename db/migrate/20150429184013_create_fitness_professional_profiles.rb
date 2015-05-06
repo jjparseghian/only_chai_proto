99140293d50f2ed7338c1f15b15af3120beeb7f1
James Parseghian <jjparseghian@gmail.com>
2015-05-05 20:12:52 -0400

    Fix field types and remove array defaults in fitness professional profiles migration

--- db/migrate/20150429184013_create_fitness_professional_profiles.rb
+++ db/migrate/20150429184013_create_fitness_professional_profiles.rb
@@ -2,21 +2,21 @@ class CreateFitnessProfessionalProfiles < ActiveRecord::Migration
   def change
     create_table :fitness_professional_profiles do |t|
       t.string :ethnicity
-      t.string :training_location
+      t.text :training_location, array: true
       t.text  :client_space_descrip
       t.text :independent_facility_descrip
       t.text :membership_facility_descrip
       t.text :private_facility_descrip
       t.text :credentials
-      t.text :goals, array: true, default: []
+      t.text :goals, array: true
       t.text :goal_specifics
-      t.text :medical_conditions, array: true, default: []
-      t.text :orthopedic_injury_experience, array: true, default: []
-      t.text :population_experience, array: true, default: []
+      t.text :medical_conditions, array: true
+      t.text :orthopedic_injury_experience, array: true
+      t.text :population_experience, array: true
       t.text :athlete_experience
-      t.text :training_style, array: true, default: []
-      t.text :appointment_lengths, array: true, default: []
-      t.text :group_training, array: true, default: []
+      t.text :training_style, array: true
+      t.text :appointment_lengths, array: true
+      t.text :group_training, array: true
       t.boolean :consent_waiver
       t.references :user, index: true
 
